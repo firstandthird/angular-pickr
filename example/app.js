83bc1d1fee7e16536054fa623a8219c2e2e20b59
Troy Whiteley <troy@somanyscientists.com>
2013-11-18 11:13:54 -0800

    Shows date pickr when input is clicked and updates date when selected.

--- example/app.js
+++ example/app.js
@@ -1,5 +1,7 @@
-angular.module('example-app', []);
+angular.module('example-app', ['pickr']);
 
 var MainController = function($scope) {
-
+  $scope.$watch('selectedDate', function(val) {
+    console.log('changed date', val);
+  }, true);
 };
