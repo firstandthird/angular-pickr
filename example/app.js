b1c0b67c9c5ebacf4c5ecd09843cf3d5090ce7e0
Greg Allen <me@jga.me>
2014-08-04 10:22:38 -0700

    updated to support pickr 0.2.0

--- example/app.js
+++ example/app.js
@@ -3,9 +3,12 @@ angular.module('example-app', ['pickr']);
 var MainController = function($scope, $timeout) {
   $scope.testModel = 'Pick Me';
   $scope.$watch('testModel', function(newVal) {
-    console.log('changed', newVal);
+    console.log('model changed', newVal);
   });
 
+  $scope.$watch('inlineModel', function(newVal) {
+    console.log('inline model changed', newVal);
+  }, true);
 
 
 };
